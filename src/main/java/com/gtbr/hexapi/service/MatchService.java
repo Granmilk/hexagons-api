c5768dc9a6fa9902c08d195fe5e87c44e62a4b5a
Granmilk <Granmilk_gt@hotmail.com>
2022-01-22 15:06:42 -0300

    fix match highscore find

--- src/main/java/com/gtbr/hexapi/service/MatchService.java
+++ src/main/java/com/gtbr/hexapi/service/MatchService.java
@@ -47,6 +47,7 @@ public class MatchService {
     public List<MatchScoreBoardRecord> findMatchList(String gameMode, Integer size) {
         return matchRepository.findByGameModeOrderByScore(gameMode, Pageable.ofSize(size)).stream()
                 .map(MatchScoreBoardRecord::from)
+                .sorted(Comparator.comparingInt(MatchScoreBoardRecord::score).reversed())
                 .toList();
     }
 
